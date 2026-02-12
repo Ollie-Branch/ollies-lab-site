52347cb7438fa987faae41fa327398b41d6e9be1
Ollie Branch <ollieevelon@gmail.com>
2026-02-12 06:40:55 -0500

    God this implementation of templating in gin is cursed; I'm saving a commit here before I go too far.

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
+
 import (
+	"text/template"
 	"net/http"
+	// "fmt"
+	"bytes"
     "github.com/gin-gonic/gin"
 )
+
+// The template that does raw response when not using HTMX
+type RawResponse struct {
+	Content string
+}
 
 func main() {
     // Create a default gin router
     router := gin.Default()
+	resp := RawResponse{"<p> \n \t Paragraph inside the content field \n </p>"}
+	buf := new(bytes.Buffer)
+	t1, err := template.New("t1").Parse("<h1> \n Test Content \n </h1> \n {{.Content}}")
+	if err != nil {
+		panic(err);
+	}
+	err = t1.Execute(buf, resp)
+	if err != nil {
+		panic(err);
+	}
+
+	// outputString := fmt.Sprintf("%s", buf)
+
 
     // Define a route for the root path
     router.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Hello World from Gin!",
-        })
-    })
+        c.Data(http.StatusOK, "text/html; charset=utf-8", buf.Bytes())
+		})
 
     // Start the server on port 8080
     router.Run(":8080")
